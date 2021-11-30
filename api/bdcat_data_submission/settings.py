91c718d0382e646c5bd5c0e9c67a893afd7d962d
Luan <ltran@asymmetrik.com>
2021-11-30 16:38:38 -0500

    fix: update ALLOWED_HOSTS

--- api/bdcat_data_submission/settings.py
+++ api/bdcat_data_submission/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]  # Instead of your actual secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
