b97fea553678c13a46f8ea64361e3b6bf500380c
Luan <ltran@asymmetrik.com>
2021-11-30 16:16:02 -0500

    chore: run black

--- api/tracker/views.py
+++ api/tracker/views.py
@@ -15,38 +15,66 @@ from .models import Ticket
 #             return HttpResponseRedirect(reverse('confirm_initiated_ticket'))
 #     else:
 #         form = SubmissionForm()
-# 
-#     return render(request, 'tracker/initiate_ticket.html', {'form': form})  
+#
+#     return render(request, 'tracker/initiate_ticket.html', {'form': form})
+
 
 class TicketCreate(CreateView):
-	model = Ticket
-	fields = ["email", "name", "organization", "study_name", "dataset_description", "is_test_data", "google_email", "aws_iam", "data_size", "study_id", "consent_code"]
+    model = Ticket
+    fields = [
+        "email",
+        "name",
+        "organization",
+        "study_name",
+        "dataset_description",
+        "is_test_data",
+        "google_email",
+        "aws_iam",
+        "data_size",
+        "study_id",
+        "consent_code",
+    ]
 
-	def form_valid(self, form):
-		ticket_obj = form.save(commit=True)
-		# FIXME - add checks here
-		return super().form_valid(form)
+    def form_valid(self, form):
+        ticket_obj = form.save(commit=True)
+        # FIXME - add checks here
+        return super().form_valid(form)
 
-# FIXME - see https://docs.djangoproject.com/en/3.2/topics/class-based-views/generic-editing/ for adding auth	
+
+# FIXME - see https://docs.djangoproject.com/en/3.2/topics/class-based-views/generic-editing/ for adding auth
 class TicketUpdate(UpdateView):
-	model = Ticket
-	fields = ["email", "name", "organization", "study_name", "dataset_description", "is_test_data", "google_email", "aws_iam", "data_size", "study_id", "consent_code"]
+    model = Ticket
+    fields = [
+        "email",
+        "name",
+        "organization",
+        "study_name",
+        "dataset_description",
+        "is_test_data",
+        "google_email",
+        "aws_iam",
+        "data_size",
+        "study_id",
+        "consent_code",
+    ]
+
 
 class TicketDelete(DeleteView):
-	model = Ticket
-	success_url = reverse_lazy('tracker:tickets-list')
+    model = Ticket
+    success_url = reverse_lazy("tracker:tickets-list")
+
 
 class TicketsList(ListView):
-	model = Ticket
+    model = Ticket
+
 
 class TicketDetail(DetailView):
-	model = Ticket
-	context_object_name = 'ticket'
+    model = Ticket
+    context_object_name = "ticket"
 
-	def get_context_data(self, **kwargs):
-		# Call the base implementation first to get a context
-		context = super().get_context_data(**kwargs)
-#		context['statuses'] = get_all_statuses(self)
-		# FIXME add status here
-		return context
-		
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # 		context['statuses'] = get_all_statuses(self)
+        # FIXME add status here
+        return context
