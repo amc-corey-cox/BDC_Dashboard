7920eb70afdc5012269797a509a81600ef6af202
Luan <ltran@asymmetrik.com>
2021-12-07 21:04:45 -0500

    feat: update ticket dashboard to columns

--- api/tracker/views.py
+++ api/tracker/views.py
@@ -3,7 +3,8 @@ from django.views.generic import DetailView, TemplateView
 from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
-from .models import Ticket
+from datetime import datetime, timezone
+from .models import Ticket, STATUS_TYPES
 
 # initiate a submission ticket
 # def create_ticket(request):
@@ -69,6 +70,53 @@ class TicketDelete(DeleteView):
 
 class TicketsList(ListView):
     model = Ticket
+    context_object_name = "tickets"
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        queryset = self.object_list
+
+        # generate a list of status types
+        context["awaiting_review"] = []
+        context["awaiting_bucket_creation"] = []
+        context["awaiting_data_upload"] = []
+        context["data_upload_in_progress"] = []
+        context["awaiting_gen3_approval"] = []
+        context["gen3_accepted"] = []
+        context["rejected"] = []
+
+        # filter tickets by status
+        for object in queryset:
+            # update ticket with status and last_updated date
+            status = object.get_ticket_status[1]
+            last_updated = (
+                datetime.now(timezone.utc) - object.get_ticket_status[0]
+            ).days
+            object.last_updated = last_updated
+
+            if status == STATUS_TYPES[1]:
+                # Awaiting Review
+                context["awaiting_review"].append(object)
+            elif status == STATUS_TYPES[2]:
+                # Awaiting Bucket Creation
+                context["awaiting_bucket_creation"].append(object)
+            elif status == STATUS_TYPES[3]:
+                # Awaiting Data Upload
+                context["awaiting_data_upload"]
+            elif status == STATUS_TYPES[4]:
+                # Data upload in Progress
+                context["data_upload_in_progress"].append(object)
+            elif status == STATUS_TYPES[5]:
+                # Awaiting Gen3 Approval
+                context["awaiting_gen3_approval"].append(object)
+            elif status == STATUS_TYPES[6]:
+                # Gen3 Accepted
+                context["gen3_accepted"].append(object)
+            else:
+                # Data Intake Form Rejected
+                context["rejected"].append(object)
+
+        return context
 
 
 class TicketDetail(DetailView):
@@ -78,9 +126,8 @@ class TicketDetail(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(DetailView, self).get_context_data(*args, **kwargs)
 
-        # Compute ticket status and add to context
-        # ticket_status = get_ticket_status(self.object)
-        # context["status"] = ticket_status[1]
-        # context["status_dt"] = ticket_status[0]
+        # add ticket status to context
+        # context["status"] = self.object.get_ticket_status[1]
+        # context["last_updated"] = self.object.get_ticket_status[0]
 
         return context
