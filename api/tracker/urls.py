c5de301337a35e5c3aad6c7faad2773fac17e9b1
Luan <ltran@asymmetrik.com>
2021-12-08 13:11:13 -0500

    demo: instruction placeholders

--- api/tracker/urls.py
+++ api/tracker/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     path("<int:pk>/delete/", views.TicketDelete.as_view(), name="ticket-delete"),
     path("list/", views.TicketsList.as_view(), name="tickets-list"),
     path("<int:pk>/detail/", views.TicketDetail.as_view(), name="ticket-detail"),
+    path("user_docs/", views.UserDocsView.as_view(), name="user-docs"),
+    path(
+        "custodian_instructions/",
+        views.CustodianInfoView.as_view(),
+        name="user-docs-detail",
+    ),
     # 	path('initiate_ticket/', views.initiate_ticket, name='initiate_ticket'),
     # 	path('confirm_initiated_ticket/', views.confirm_initiated_ticket, name='confirm_initiated_ticket'),
     #    path('detail/', views.detail, name='detail'),
