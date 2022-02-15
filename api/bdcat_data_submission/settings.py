9c4cf803e5362da605004d4fadcad35881558cc2
Luan <ltran@asymmetrik.com>
2022-02-15 13:31:36 -0500

    feat: add login template override

--- api/bdcat_data_submission/settings.py
+++ api/bdcat_data_submission/settings.py
@@ -94,7 +94,10 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
