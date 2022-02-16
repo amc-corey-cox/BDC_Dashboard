2215fdaece80478cebf6bcbb3b0aaa461ce9ca3f
Luan <ltran@asymmetrik.com>
2022-02-16 15:57:46 -0500

    fix: update sendgrid templates

--- api/tracker/mail.py
+++ api/tracker/mail.py
@@ -1,10 +1,11 @@
 import os
 from django.conf import settings
 from django.core.mail import EmailMessage
+from .models import STATUS_TYPES
 
 
 # admin emails (add more here)
-NO_REPLY_EMAIL = "ann@nimbusinformatics.com"
+NO_REPLY_EMAIL = settings.SENDGRID_NO_REPLY_EMAIL
 ADMIN_EMAIL = settings.SENDGRID_ADMIN_EMAIL
 
 
@@ -13,12 +14,13 @@ SENDGRID_TICKET_CREATED_TEMPLATE_ID_USER = "d-d4c3414e680e415ba59c9fecc760ead4"
 SENDGRID_TICKET_DELETED_TEMPLATE_ID_USER = "d-88ccfe3f976940e6ae8d5dd6c1d7ab65"
 SENDGRID_TICKET_REJECTED_TEMPLATE_ID_USER = "d-3e07572d9d29445e8763aa7c8c98fc1d"
 SENDGRID_TICKET_UPDATED_TEMPLATE_ID_USER = "d-c8b625239d834a3aac57d84e1b9ea340"
+SENDGRID_BUCKET_CREATED_TEMPLATE_ID_USER = "d-f485f19f51ea48758ef41ba8b56a3c90"
 
-# sendgrid templates for admins (TODO: replace these)
-SENDGRID_TICKET_CREATED_TEMPLATE_ID_ADMIN = "d-d4c3414e680e415ba59c9fecc760ead4"
-SENDGRID_TICKET_DELETED_TEMPLATE_ID_ADMIN = "d-88ccfe3f976940e6ae8d5dd6c1d7ab65"
-SENDGRID_TICKET_REJECTED_TEMPLATE_ID_ADMIN = "d-3e07572d9d29445e8763aa7c8c98fc1d"
-SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN = "d-c8b625239d834a3aac57d84e1b9ea340"
+# sendgrid templates for admins
+SENDGRID_TICKET_CREATED_TEMPLATE_ID_ADMIN = "d-8150144b88434ccea5acda12857793d0"
+SENDGRID_TICKET_DELETED_TEMPLATE_ID_ADMIN = "d-4429c4f8335d4185bc96cda80305055d"
+SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN = "d-4305969f01f84d699d14da0bffa8dbd6"
+SENDGRID_DATA_UPLOAD_COMPLETED_TEMPLATE_ID_ADMIN = "d-22bce004d5f94d7f9649cab2156e767c"
 
 
 class Mail(EmailMessage):
@@ -40,7 +42,6 @@ class Mail(EmailMessage):
         if status == "Created":
             self.template_id = SENDGRID_TICKET_CREATED_TEMPLATE_ID_USER
 
-            # TODO: these are just placeholders
             self.admin_email.template_id = SENDGRID_TICKET_CREATED_TEMPLATE_ID_ADMIN
             self.admin_email.to = [ADMIN_EMAIL]
 
@@ -50,28 +51,45 @@ class Mail(EmailMessage):
         elif status == "Deleted":
             self.template_id = SENDGRID_TICKET_DELETED_TEMPLATE_ID_USER
 
-            # TODO: these are just placeholders
             self.admin_email.template_id = SENDGRID_TICKET_DELETED_TEMPLATE_ID_ADMIN
             self.admin_email.to = [ADMIN_EMAIL]
 
             # attach other data here
 
-        # rejected message
-        elif status == "Data Intake Form Rejected":
+        # rejected message: Data Intake Form Rejected
+        elif status == STATUS_TYPES[0]:
             self.template_id = SENDGRID_TICKET_REJECTED_TEMPLATE_ID_USER
 
-            # TODO: these are just placeholders
-            self.admin_email.template_id = SENDGRID_TICKET_REJECTED_TEMPLATE_ID_ADMIN
+            self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
             self.admin_email.to = [ADMIN_EMAIL]
 
             # attach rejected_reason
             dynamic_template_data["rejected_reason"] = ticket.ticket_review_comment
 
+        # bucket created message: Awaiting Data Custodian Upload Start
+        elif status == STATUS_TYPES[3]:
+            self.template_id = SENDGRID_BUCKET_CREATED_TEMPLATE_ID_USER
+
+            self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
+            self.admin_email.to = [ADMIN_EMAIL]
+
+            # attach other data here
+
+        # data upload completed message: Awaiting Gen3 Acceptance
+        elif status == STATUS_TYPES[5]:
+            self.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_USER
+
+            self.admin_email.template_id = (
+                SENDGRID_DATA_UPLOAD_COMPLETED_TEMPLATE_ID_ADMIN
+            )
+            self.admin_email.to = [ADMIN_EMAIL]
+
+            # attach other data here
+
         # update message
         else:
             self.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_USER
 
-            # TODO: these are just placeholders
             self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
             self.admin_email.to = [ADMIN_EMAIL]
 
