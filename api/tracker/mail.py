94eadb069b3d3947fadd23fbb99ae871cc50ab80
Ann Van <ann.van@gmail.com>
2022-09-12 22:01:17 -0700

    UI cleanup and added debugging for mail

--- api/tracker/mail.py
+++ api/tracker/mail.py
@@ -2,7 +2,12 @@ import os
 from django.conf import settings
 from django.core.mail import EmailMessage
 from .models import STATUS_TYPES
+from smtplib import SMTPException
+from django.core.mail import BadHeaderError
+from python_http_client import exceptions
+import logging
 
+logger = logging.getLogger("django")
 
 # admin emails (add more here)
 NO_REPLY_EMAIL = settings.SENDGRID_NO_REPLY_EMAIL
@@ -23,74 +28,58 @@ SENDGRID_DATA_UPLOAD_COMPLETED_TEMPLATE_ID_ADMIN = settings.SENDGRID_DATA_UPLOAD
 
 class Mail(EmailMessage):
     def __init__(self, ticket, status):
-        ticket_url = os.environ.get("APP_ENGINE_URL")
-
+        ticket_url = os.environ.get("AZURE_SITES_URL")
+        logger.info("status = " + status)
         # general dynamic data for all emails
         dynamic_template_data = {
             "name": ticket.name,
             "study_name": ticket.study_name,
-            "ticket_url": f"{ticket_url}{ticket.id}/update",
+            "ticket_url": f"{ticket_url}/{ticket.id}/update",
         }
 
         # create email message object and fill in the details
         # in tandem with the user's email
         self.admin_email = EmailMessage(from_email=NO_REPLY_EMAIL)
-
+        self.admin_email.to = [ADMIN_EMAIL]
+		
         # create message
         if status == "Created":
             self.template_id = SENDGRID_TICKET_CREATED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = SENDGRID_TICKET_CREATED_TEMPLATE_ID_ADMIN
-            self.admin_email.to = [ADMIN_EMAIL]
-
             # attach other data here
 
         # delete message
         elif status == "Deleted":
             self.template_id = SENDGRID_TICKET_DELETED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = SENDGRID_TICKET_DELETED_TEMPLATE_ID_ADMIN
-            self.admin_email.to = [ADMIN_EMAIL]
-
             # attach other data here
 
         # rejected message: Data Intake Form Rejected
         elif status == STATUS_TYPES[0]:
             self.template_id = SENDGRID_TICKET_REJECTED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
-            self.admin_email.to = [ADMIN_EMAIL]
-
             # attach rejected_reason
             dynamic_template_data["rejected_reason"] = ticket.ticket_review_comment
 
         # bucket created message: Awaiting Data Custodian Upload Start
         elif status == STATUS_TYPES[3]:
             self.template_id = SENDGRID_BUCKET_CREATED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
-            self.admin_email.to = [ADMIN_EMAIL]
-
             # attach other data here
 
         # data upload completed message: Awaiting Gen3 Acceptance
         elif status == STATUS_TYPES[5]:
             self.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = (
                 SENDGRID_DATA_UPLOAD_COMPLETED_TEMPLATE_ID_ADMIN
             )
-            self.admin_email.to = [ADMIN_EMAIL]
 
             # attach other data here
 
         # update message
         else:
             self.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_USER
-
             self.admin_email.template_id = SENDGRID_TICKET_UPDATED_TEMPLATE_ID_ADMIN
-            self.admin_email.to = [ADMIN_EMAIL]
-
             # attach ticket_status
             dynamic_template_data["ticket_status"] = status
 
@@ -103,6 +92,22 @@ class Mail(EmailMessage):
         )
 
     def send(self, fail_silently=False):
+        result = 0	  
         # send emails out to the admin
-        self.admin_email.send(fail_silently)
-        return super().send(fail_silently)
+        try:   
+        	logger.info("Attempting to send admin email.")     
+        	self.admin_email.send(fail_silently)
+        	logger.info("Attempting to send email data custodian email.")             
+        	result = super().send(fail_silently)
+        # SendGrid email error handling: https://github.com/sendgrid/sendgrid-python/blob/main/use_cases/error_handling.md        	
+        except BadHeaderError:             
+	        logger.error('Invalid header found for email.')
+        except SMTPException as e: 
+	        logger.error('SMTP exception when sending email: ' + e)
+        except exceptions.BadRequestsError as e:
+	        logger.error('BadRequestsHeader for email.')
+	        logger.error(e.body)        
+        except:  
+	        logger.error("Mail Sending Failed for email to")  
+	            
+        return result
