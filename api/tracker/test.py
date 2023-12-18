0a68546d9fe613cccef01edf0252430408d07b52
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-12-18 10:15:35 -0600

    Add Tracker Views tests

--- api/tracker/test.py
+++ api/tracker/test.py
@@ -1,5 +1,8 @@
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse, resolve
+
+from django.contrib.auth import get_user_model
+
 from .views import TicketsList, UserProfile, TicketDetail, DocumentationView, AboutView
 
 
@@ -25,9 +28,57 @@ class UrlsTest(SimpleTestCase):
         self.assertEqual(resolve(url).func.view_class, AboutView)
 
 
-class DocumentationViewTest(TestCase):
-    def test_template_rendering(self):
+class ViewsTest(TestCase):
+    def setUp(self):
+        user_model = get_user_model()
+        self.user = user_model.objects.create_user(email='test@email.com', password='test_password')
+
+    def test_documentation_view(self):
         url = reverse('tracker:documentation')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base_generic.html')
         self.assertTemplateUsed(response, 'tracker/user_docs.html')
+
+    def test_about_view(self):
+        url = reverse('tracker:about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base_generic.html')
+        self.assertTemplateUsed(response, 'tracker/about.html')
+
+    def test_ticket_list_view(self):
+        url = reverse('tracker:tickets-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/accounts/login/?next=/')
+        redirected_response = self.client.get(response.url)
+        self.assertEqual(redirected_response.status_code, 200)
+
+        self.client.login(email='test@email.com', password='test_password')
+
+        auth_response = response = self.client.get(url)
+        self.assertEqual(auth_response.status_code, 200)
+        self.assertTemplateUsed(response, 'base_generic.html')
+        self.assertTemplateUsed(response, 'tracker/ticket_list.html')
+
+    def test_ticket_detail_view(self):
+        url = reverse('tracker:ticket-detail', kwargs={'pk': 'BDJW-123'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/accounts/login/?next=/BDJW-123/detail/')
+        redirected_response = self.client.get(response.url)
+
+        self.client.login(email='test@email.com', password='test_password')
+
+        auth_response = response = self.client.get(url)
+        self.assertEqual(auth_response.status_code, 200)
+        self.assertTemplateUsed(response, 'base_generic.html')
+        self.assertTemplateUsed(response, 'tracker/ticket_detail.html')
+
+    def test_profile_view(self):
+        url = reverse('tracker:profile')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base_generic.html')
+        self.assertTemplateUsed(response, 'tracker/profile.html')
